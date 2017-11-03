3513b9c6a54056d79cf47a45c864823adc9c8512
harrypotter0 <9654263057akashkandpal@gmail.com>
2017-11-03 13:11:31 +0530

    menu added

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
