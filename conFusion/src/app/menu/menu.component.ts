0ec83bd82a03cb42f01d1437616bdd0ad72f2dae
harrypotter0 <9654263057akashkandpal@gmail.com>
2017-11-12 13:11:41 +0530

    checking atom :)

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -9,7 +9,7 @@ const DISHES: Dish[]= [
                          category: 'mains',
                          label:'Hot',
                          price:'4.99',
-                         description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+                         description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer .'                        },
                       {
                          name:'Zucchipakoda',
                          image: '/assets/images/zucchipakoda.png',
